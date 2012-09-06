c0a225d1220668aaf5bf2168c95c37b3cc2abb09
bmiro <bartomeumiro@gmail.com>
2012-09-06 17:09:23 +0545

    Arg parsing on the server

--- apt-transfer-client.py
+++ apt-transfer-client.py
@@ -1,16 +1,34 @@
 #!/usr/bin/env python3
 #-*- coding: utf-8 -*-
 
+##########################################################################
+#   This program is free software: you can redistribute it and/or modify #
+# it under the terms of the GNU General Public License as published by   #
+# the Free Software Foundation, either version 3 of the License, or      #
+# (at your option) any later version.                                    #
+#                                                                        #
+#   This program is distributed in the hope that it will be useful,      #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of         #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          #
+# GNU General Public License for more details.                           #
+#                                                                        #
+# You should have received a copy of the GNU General Public License      #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.  #
+##########################################################################
+
+
 import sys
 import shlex
 import subprocess
 import urllib.request
 
-usage_text = "apt-transfer-client server_url \n \
-\t i.e.: apt-transfer-client 192.168.254.1/apt-transfer.\n"
-
 PACKAGE_LIST_NAME = "package.list"
 SOURCES_LIST_BACKUP_NAME = "sources.list.apt-transfer.bak"
+SOURCES_LIST_PATH = "/etc/apt"
+
+def print_help():
+    print("apt-transfer-client server_url \n \
+\t i.e.: apt-transfer-client 192.168.254.1/apt-transfer.")
 
 def override_sources_list(server_url):
     pass 
--- apt-transfer-server.py
+++ apt-transfer-server.py
@@ -3,14 +3,43 @@
 
 from sys import argv
 
+##########################################################################
+#   This program is free software: you can redistribute it and/or modify #
+# it under the terms of the GNU General Public License as published by   #
+# the Free Software Foundation, either version 3 of the License, or      #
+# (at your option) any later version.                                    #
+#                                                                        #
+#   This program is distributed in the hope that it will be useful,      #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of         #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          #
+# GNU General Public License for more details.                           #
+#                                                                        #
+# You should have received a copy of the GNU General Public License      #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.  #
+##########################################################################
+
+
 
 VERSION="pre-alpha"
-AVALIABLE_SECTIONS=["main", "contrib", "non-free",          # Debian
-                    "universe", "multiverse", "restricted"] # Ubuntu specific
 
-# apt-transfer-server mirror http://ftp.debian.org/debian/ testing main contrib non-free --dest-path /var/www/debian-mirror
-MIRROR_REGEX=r""
-UPDATE_REGEX=r""
+SOURCES_VERSIONS=[# Debian
+                 "oldstable", "stable", "testing", "unstable", "experimental",\
+                 "buzz", "rex", "bo", "hamm", "slink", "potato","woody", \
+                 "sarge", "etch", "lenny", "squeeze", "wheezy", "jessie",\
+                 "sid",\
+                 # Ubuntu
+                 "warty", "hoary", "breezy", "dapper", "edgy", "feisty", \
+                 "gutsy", "hardy", "intrepid", "jaunty", "karmic", \
+                 "maverick", "lucid", "natty", "oneiric", "precise", "quantal"]
+
+SOURCES_SECTIONS=["main", "contrib", "non-free",          # Debian
+                  "universe", "multiverse", "restricted"] # Ubuntu specific
+
+COMMAND_LIST = ["mirror", "initialize", "update", "start", "stop", "clean"]
+
+# apt-transfer-server mirror http://ftp.debian.org/debian/ \
+#                            testing main contrib non-free \
+#                            --dest-path /var/www/debian-mirror
 
 
 def print_help():
@@ -40,7 +69,51 @@ at the source with\n\t\t\tthe same sintax as sources.list to the given path.")
 """
 def arg_parsing(arg_v):
 
+    print arg_v
+
+    command = arg_v[1]
+
+    if not command in COMMAND_LIST:
+        return {"error" : "I don't understand the command " + command}
+
+
+    if command == "mirror":
+        url = arg_v[2]
+        if not validate_url(url):
+            return {"error" : "I don't understand the mirror url " + url}
+
+        version = arg_v[3]
+        if not version in SOURCES_VERSION:
+            return {"error" : "I don't understand the version " + version}
+
+        sections = []
+        i = 4 # The current index of the arg_v
+        while arg_v[i] in SOURCES_SECTION:
+            sections += arg_v[i]
+
+        if not sections:
+            return {"error" : "I don't detect any software section (ie main)"}
+
+
+    elif command == "initialize":
+
+    elif command == "update":
+
+    elif command == "start":
+
+    elif command == "stop":
+
+    elif command == "clean":
+        
+
+    
+
+
     return None
+
+
+def validate_url(url):
+    return True
 
 
 def mirror():
@@ -67,7 +140,7 @@ if __name__=='__main__':
 
     arg = arg_parsing(argv)
 
-    if not arg:
+    if "error" in arg:
         print_help()
         exit()
 
